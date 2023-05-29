1131a8b2bc83da85e2daed72f8c8a4077a787906
Elroi Yoshua <elroiyoshua@student.telkomuniversity.ac.id>
2023-05-29 10:11:49 +0700

    api
    nambah api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,6 +29,10 @@ class UserController extends Controller
     public function index()
     {
         $karyawan = Users::with('lembur', 'cuti', 'departement')->get();
+        if(request()-> segment(1) =='api') return response()->json([
+            "error"=> false,
+            "list" => $karyawan,
+        ]);
         return view('karyawan.index', ['data' => $karyawan]);
     }
 
